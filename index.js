c8ee35d3e801cca3d24d83a9feecb6d9a90309c7
Usama <usama.bashirb1@gmail.com>
2022-01-20 20:30:54 +0500

    Changes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 
 const db = mysql.createPool({
     host: process.env.DB_HOST_FREE,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER_FREE,
     password: process.env.DB_PASSWORD_FREE,
     database: process.env.DB_NAME_FREE
